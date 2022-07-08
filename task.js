176d1608d7e3e2ad819abf213a4d5e89bc80c9ef
Dan <daysprng@gmail.com>
2022-07-08 17:30:07 +0300

    initial commit

---
+++ task.js
@@ -0,0 +1,36 @@
+function Student(name, gender, age) {
+    this.name = name,
+    this.gender = gender,
+    this.age = age
+}
+
+Student.prototype.setSubject = function (subjectName) {
+  this.subject = subjectName
+}
+
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+    this.marks = [mark]
+    } else {
+      this.marks.push(mark)
+    }
+  
+}
+
+Student.prototype.addMarks = function (...marks) {
+  if(this.marks === undefined){ 
+    this.marks = marks
+    } else {
+      this.marks = [...this.marks, ...marks]
+    }
+}
+
+Student.prototype.getAverage = function () {
+  return this.marks.reduce((a,b) => a + b, 0) / this.marks.length
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
+}
